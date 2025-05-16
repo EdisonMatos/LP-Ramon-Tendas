624136a135b67e4a775ff50f37392448a1b4d518
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-16 09:25:05 -0300

    fix: Insere infos no footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -36,6 +36,38 @@ function Footer() {
                 {infos.phone}
               </a>
             </div>
+
+            <div className="flex full gap-x-[8px] items-center opacity-70">
+              <Phone
+                size={24}
+                aria-label="Ícone de Telefone"
+                strokeWidth={1}
+                className="text-white"
+              />
+              <a
+                href={whatsappContactLink}
+                target="_blank"
+                className="hover:underline"
+              >
+                {infos.phoneTwo}
+              </a>
+            </div>
+
+            <div className="flex full gap-x-[8px] items-center opacity-70">
+              <Phone
+                size={24}
+                aria-label="Ícone de Telefone"
+                strokeWidth={1}
+                className="text-white"
+              />
+              <a
+                href={whatsappContactLink}
+                target="_blank"
+                className="hover:underline"
+              >
+                {infos.phoneThree}
+              </a>
+            </div>
             <div className="flex full gap-x-[8px] items-center opacity-70">
               <Mail
                 size={24}
@@ -45,98 +77,18 @@ function Footer() {
               />
               <p>{infos.mail}</p>
             </div>
-            <p className="opacity-70">Frase de observação:</p>
-            <div className="opacity-70 invert">
-              {" "}
-              <div className="flex gap-[10px] items-center">
-                <a
-                  href={links.socialMedia.instagram}
-                  target="_blank"
-                  aria-label="Link para o Instagram"
-                >
-                  <IconButton
-                    icon={
-                      <svg
-                        aria-label="Botão para o Instagram"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="lucide lucide-instagram"
-                      >
-                        <rect
-                          width="20"
-                          height="20"
-                          x="2"
-                          y="2"
-                          rx="5"
-                          ry="5"
-                        />
-                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-                      </svg>
-                    }
-                  />
-                </a>
-                <a
-                  href={links.socialMedia.facebook}
-                  target="_blank"
-                  aria-label="Link para o Instagram"
-                >
-                  <IconButton
-                    icon={
-                      <svg
-                        aria-label="Botão para o Facebook"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="lucide lucide-facebook"
-                      >
-                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                      </svg>
-                    }
-                  />
-                </a>
-                <a
-                  href={links.socialMedia.linkedin}
-                  target="_blank"
-                  aria-label="Link para o Instagram"
-                >
-                  <IconButton
-                    icon={
-                      <svg
-                        aria-label="Botão para o Linkedin"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="lucide lucide-linkedin"
-                      >
-                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-                        <rect width="4" height="12" x="2" y="9" />
-                        <circle cx="4" cy="4" r="2" />
-                      </svg>
-                    }
-                  />
-                </a>
-              </div>
+
+            <div className="flex full gap-x-[8px] items-center opacity-70">
+              <Mail
+                size={24}
+                aria-label="Ícone de Carta"
+                strokeWidth={1}
+                className="text-white"
+              />
+              <p>{infos.mailTwo}</p>
             </div>
+            {/* <p className="opacity-70">Frase de observação:</p> */}
+            <div className="opacity-70 invert"> </div>
           </div>
         </MotionDivDownToUp>
         <MotionDivDownToUp>
@@ -145,6 +97,87 @@ function Footer() {
               ATÉ MAIS!
             </h1>
             <p className="opacity-70">{infos.footerInfos.footerText}</p>
+          </div>
+          <div className="flex gap-[10px] pt-8 items-center">
+            <a
+              href={links.socialMedia.instagram}
+              target="_blank"
+              aria-label="Link para o Instagram"
+            >
+              <IconButton
+                icon={
+                  <svg
+                    aria-label="Botão para o Instagram"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-instagram"
+                  >
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                    <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                  </svg>
+                }
+              />
+            </a>
+            <a
+              href={links.socialMedia.facebook}
+              target="_blank"
+              aria-label="Link para o Instagram"
+            >
+              <IconButton
+                icon={
+                  <svg
+                    aria-label="Botão para o Facebook"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-facebook"
+                  >
+                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                  </svg>
+                }
+              />
+            </a>
+            <a
+              href={links.socialMedia.linkedin}
+              target="_blank"
+              aria-label="Link para o Instagram"
+            >
+              <IconButton
+                icon={
+                  <svg
+                    aria-label="Botão para o Linkedin"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-linkedin"
+                  >
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                    <rect width="4" height="12" x="2" y="9" />
+                    <circle cx="4" cy="4" r="2" />
+                  </svg>
+                }
+              />
+            </a>
           </div>
         </MotionDivDownToUp>
         <MotionDivDownToUp>
--- src/content/infos.jsx
+++ src/content/infos.jsx
@@ -1,8 +1,11 @@
 const infos = {
   name: "A_Definir",
   slogan: "A_Definir",
-  phone: "(00) 00000-0000",
-  mail: "A_Definir",
+  phone: "(62) 99287-3590",
+  phoneTwo: "(62) 99334-2781",
+  phoneThree: "(62) 99274-5417",
+  mail: "ramomtendas01@gmail.com",
+  mailTwo: "ramomtendas02@gmail.com",
   footerInfos: {
     year: 2024,
     footerText:
