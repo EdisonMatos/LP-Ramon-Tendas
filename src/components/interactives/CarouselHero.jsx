95d788768c2dd67faf97ccdd740f25fef46aaee6
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-16 11:56:04 -0300

    fix: Estiliza imagens da Hero

--- src/components/interactives/CarouselHero.jsx
+++ src/components/interactives/CarouselHero.jsx
@@ -140,41 +140,41 @@ const CarouselHero = () => {
         autoplay={false}
         className="mb-[80px]"
       >
-        <div className="carousel-item">
-          <img src={imgTestimonial1} alt="Testimonial 1" />
+        <div className="carousel-item ">
+          <img src={imgTestimonial1} className="rounded-[5px] border" alt="Testimonial 1" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial2} alt="Testimonial 2" />
+          <img src={imgTestimonial2} className="rounded-[5px] border" alt="Testimonial 2" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial3} alt="Testimonial 3" />
+          <img src={imgTestimonial3} className="rounded-[5px] border" alt="Testimonial 3" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial4} alt="Testimonial 4" />
+          <img src={imgTestimonial4} className="rounded-[5px] border" alt="Testimonial 4" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial5} alt="Testimonial 5" />
+          <img src={imgTestimonial5} className="rounded-[5px] border" alt="Testimonial 5" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial6} alt="Testimonial 6" />
+          <img src={imgTestimonial6} className="rounded-[5px] border" alt="Testimonial 6" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial7} alt="Testimonial 7" />
+          <img src={imgTestimonial7} className="rounded-[5px] border" alt="Testimonial 7" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial8} alt="Testimonial 8" />
+          <img src={imgTestimonial8} className="rounded-[5px] border" alt="Testimonial 8" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial9} alt="Testimonial 9" />
+          <img src={imgTestimonial9} className="rounded-[5px] border" alt="Testimonial 9" />
         </div>
         {/* <div className="carousel-item">
-          <img src={imgTestimonial10} alt="Testimonial 10" />
+          <img src={imgTestimonial10 className="rounded-[5px] border"} alt="Testimonial 10" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial11} alt="Testimonial 11" />
+          <img src={imgTestimonial11 className="rounded-[5px] border"} alt="Testimonial 11" />
         </div>
         <div className="carousel-item">
-          <img src={imgTestimonial12} alt="Testimonial 12" />
+          <img src={imgTestimonial12 className="rounded-[5px] border"} alt="Testimonial 12" />
         </div> */}
       </Carousel>
 
