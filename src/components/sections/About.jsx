7e9778c6a2a7e3d2eaca19d6bfc271d0dfd59d75
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-16 11:59:45 -0300

    fix: Insere a galeria no about

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,8 +8,8 @@ import { Dialog } from "primereact/dialog";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import content from "../../content/content"; // ✅ Corrigido
-import { abstractions } from "../../content/content"; // ✅ Corrigido
+import content from "../../content/content";
+import { abstractions } from "../../content/content";
 import Button from "../interactives/Button";
 import SectionHeader from "../sectionElements/SectionHeader";
 import imgPoints from "../../assets/imgs/about/points.png";
@@ -18,6 +18,17 @@ import SectionShapeDiv from "../sectionElements/SectionShapeDiv";
 import { MoveRight } from "lucide-react";
 import { FaWhatsapp } from "react-icons/fa";
 import links from "../../content/links";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import imgTestimonial1 from "../../assets/imgs/hero/heroImg1.png";
+import imgTestimonial2 from "../../assets/imgs/hero/heroImg2.png";
+import imgTestimonial3 from "../../assets/imgs/hero/heroImg3.png";
+import imgTestimonial4 from "../../assets/imgs/hero/heroImg4.png";
+import imgTestimonial5 from "../../assets/imgs/hero/heroImg5.png";
+import imgTestimonial6 from "../../assets/imgs/hero/heroImg6.png";
+import imgTestimonial7 from "../../assets/imgs/hero/heroImg7.png";
+import imgTestimonial8 from "../../assets/imgs/hero/heroImg8.png";
+import imgTestimonial9 from "../../assets/imgs/hero/heroImg9.png";
 
 const whatsappContactLink = `${links.ctaWhatsapp}`;
 
@@ -25,6 +36,45 @@ export default function About() {
   const [visible, setVisible] = useState(false);
   const [modalContent, setModalContent] = useState(null);
   const [modalTitle, setModalTitle] = useState("");
+
+  const images = [
+    {
+      original: imgTestimonial1,
+      thumbnail: imgTestimonial1,
+    },
+    {
+      original: imgTestimonial2,
+      thumbnail: imgTestimonial2,
+    },
+    {
+      original: imgTestimonial3,
+      thumbnail: imgTestimonial3,
+    },
+    {
+      original: imgTestimonial4,
+      thumbnail: imgTestimonial4,
+    },
+    {
+      original: imgTestimonial5,
+      thumbnail: imgTestimonial5,
+    },
+    {
+      original: imgTestimonial6,
+      thumbnail: imgTestimonial6,
+    },
+    {
+      original: imgTestimonial7,
+      thumbnail: imgTestimonial7,
+    },
+    {
+      original: imgTestimonial8,
+      thumbnail: imgTestimonial8,
+    },
+    {
+      original: imgTestimonial9,
+      thumbnail: imgTestimonial9,
+    },
+  ];
 
   const onClick = () => {
     setModalTitle(abstractions.titleModal);
@@ -51,13 +101,72 @@ export default function About() {
   return (
     <SectionArea id="about" className="bg-bgSectionDark">
       <SectionWrapper className="flex flex-col desktop1:flex-row-reverse gap-[40px] desktop1:gap-x-[40px] desktop2:gap-0 desktop1:justify-between">
-        <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
+        {/* <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
           <div
             style={{
               backgroundImage: `url(${content.texts.about.imagem.img})`,
             }}
             className="shadow-custom-opacity shadow-secondary/25 relative bg-center bg-no-repeat bg-cover h-[350px] w-[90%] tablet1:w-full tablet1:h-[800px] desktop1:h-[467px] rounded-xl"
           ></div>
+        </MotionDivDownToUp> */}
+
+        <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
+          <div
+            // style={{
+            //   backgroundImage: `url(${content.texts.about.imagem.img})`,
+            // }}
+            className="shadow-custom-opacity shadow-secondary/25 relative bg-center bg-no-repeat bg-cover h-[350px] w-[90%] tablet1:w-full tablet1:h-[740px] desktop1:h-[467px] rounded-xl"
+          >
+            <ImageGallery
+              items={images}
+              showNav={false} // Ativando a navegação
+              showFullscreenButton={false} // Desativando botão de tela cheia
+              useBrowserFullscreen={false} // Desativando o uso de tela cheia do navegador
+              showBullets={true}
+              showThumbnails={false}
+              additionalClass="custom-gallery"
+              autoPlay={true}
+            />
+            <style>
+              {`
+                    .custom-gallery .image-gallery-slide img {
+                      height: 350px; 
+                      width: 100%;
+                      object-fit: cover;
+                      border-radius: 10px;
+                    }
+
+                     @media (min-width: 640px) and (max-width: 1023px) {
+                        .custom-gallery .image-gallery-slide img {
+                          height: 800px;
+                        }
+                      }
+
+                      @media (min-width: 1024px) {
+                        .custom-gallery .image-gallery-slide img {
+                          height: 470px;
+                        }
+                      }
+
+
+                    .custom-gallery .image-gallery-thumbnails img {
+                      height: 60px;  
+                      width: 100px;  
+                      object-fit: cover; 
+                    }
+                  `}
+            </style>
+            {/* <img
+              alt="Imagem de efeito pontilhado"
+              src={imgPoints}
+              className="absolute opacity-30 right-[-10px] top-[20px] desktop1:right-[-40px] desktop1:top-[40px]"
+            ></img> */}
+          </div>
+          {/* <img
+            src={content.texts.about.imagem.img}
+            alt="Imagem de uma praia de Arraial"
+            className=""
+        /> */}
         </MotionDivDownToUp>
 
         <div className="desktop1:w-[550px] desktop2:w-[570px]">
